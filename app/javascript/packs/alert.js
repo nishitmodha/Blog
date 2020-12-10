23ebc1d2788265c5dc2a3c9906cae5fa302b98a2
Nishit Modha <nishitmodha30@gmail.com>
2020-12-10 21:29:13 +0530

    Made Navbar responsive

--- app/javascript/packs/alert.js
+++ app/javascript/packs/alert.js
@@ -7,3 +7,16 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 });
+
+
+$(document).ready(function() {
+
+  // Check for click events on the navbar burger icon
+  $(".navbar-burger").click(function() {
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      $(".navbar-burger").toggleClass("is-active");
+      $(".navbar-menu").toggleClass("is-active");
+
+  });
+});
